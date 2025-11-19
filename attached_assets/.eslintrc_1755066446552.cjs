db530135eb692c0d24d2951b4ca4d72ddb1aaf0c
colcamenterprises-collab <colcamenterprises@gmail.com>
2025-11-19 14:40:23 +0700

    Add attached_assets/.eslintrc_1755066446552.cjs

---
+++ attached_assets/.eslintrc_1755066446552.cjs
@@ -0,0 +1,15 @@
+// .eslintrc.cjs
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "next/core-web-vitals"
+  ],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "no-console": "off"
+  }
+};
